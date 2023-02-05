cc5429da72dd5ffe8d85d31a590fa7c64cced42e
Boniface Bassey <bbasseyboniface@gmail.com>
2023-02-05 11:40:37 +0100

    Implemented pin interrupt to be used for motion detection

---
+++ components/motion/include/motion.h
@@ -0,0 +1,42 @@
+#ifndef __MOTION_H__
+#define __MOTION_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef CONFIG_ENABLE_MOTION_DETECTION
+
+    /**
+     * @brief motion detection callback function type
+     * @param event     : Event type
+     * @param motion    : Point to callback value
+     **/
+    typedef void (*motion_event_cb_t)(bool motion);
+
+    /**
+     * @brief           This function is called to register a motion detection event
+     *
+     * @param[in]       callback: callback function
+     */
+    void motion_register_callback(motion_event_cb_t callback);
+
+    /**
+     * @brief           This function is called to unregister a motion detection  event
+     */
+    void motion_unregister_callback();
+
+    /**
+     * @brief           This function is called to initialize the motion detection event
+     * @note            It's recommended to call motion_register_callback(motion_event_cb_t callback) before calling this function
+     */
+    void motion_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONFIG_ENABLE_MOTION_DETECTION */
+
+#endif /* __MOTION_H__ */
--- components/sdcard/include/sdcard.h
+++ components/sdcard/include/sdcard.h
@@ -1,12 +1,21 @@
-#ifndef INCLUDED_SDCARD_H_
-#define INCLUDED_SDCARD_H_
+#ifndef __SDCARD_H__
+#define __SDCARD_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 #include "esp_err.h"
 
-esp_err_t sdcard_init();
+    esp_err_t sdcard_init();
 
-esp_err_t sdcard_saveImage(unsigned char *buffer, size_t size);
+    esp_err_t sdcard_saveImage(unsigned char *buffer, size_t size);
 
-void sdcard_deleteImage(const char *filename);
+    void sdcard_deleteImage(const char *filename);
 
-#endif /* INCLUDED_SDCARD_H_ */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SDCARD_H__ */
